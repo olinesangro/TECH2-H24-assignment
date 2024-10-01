428823abed5139a4e6eab8262a07f7db2fcfbd56
olinesangro <Oline.Sangro@student.nhh.no>
2024-10-01 16:14:45 +0200

    3/3 parts done in task A

--- part_A.py
+++ part_A.py
@@ -1,13 +1,28 @@
 
 #TECH2 mandatory assignment - Part A
 
-
 from math import sqrt #Import the built-in function in order to compute the square root
 
 num_lst= [1,2,3,4,5]
-N = len(num_lst)
 
-def std_dev(x):
+
+#FOR LOOPS
+
+def std_loops(x):
+    """
+    
+
+    Parameters
+    ----------
+    x : sequence of numbers
+
+    Returns
+    -------
+    Float
+        Standard deviation of the list of numbers
+
+    """
+    N = len(num_lst)
     
     #Calculate the mean:
     total = 0
@@ -25,4 +40,52 @@ def std_dev(x):
     #Calculate the standard deviation
     return sqrt(var)
 
-print(std_dev(num_lst))
+print(std_loops(num_lst))
+
+
+
+#BUILT_IN FUNCTIONS: SUM and LEN
+
+def std_builtin(x):
+    """
+    
+
+    Parameters
+    ----------
+    x : sequence of numbers
+
+    Returns
+    -------
+    float
+        Standard deviation of the list of numbers
+
+    """
+
+    N = len(x)
+    
+    #Calculate the mean
+    mean = sum(x)/N
+
+    #Calculate the mean of squares
+    mean_of_squares = sum(num**2 for num in x)/N
+
+    #Calculate the variance
+    var = mean_of_squares - mean ** 2
+
+    #Calculate the standard deviation
+    return sqrt(var)
+
+print('Standard deviation using built in fuctions:' , std_builtin(num_lst))
+    
+
+
+
+#NUMPY
+import numpy as np
+
+def std_numpy(x):
+    return np.std(x)
+
+print('Standard deviation using NumPY:', std_numpy(num_lst))
+
+    
