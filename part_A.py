62ea84ee715eef436a5845bb36e3223e86844bd7
olinesangro <Oline.Sangro@student.nhh.no>
2024-10-01 15:11:13 +0200

    Changes 2

--- part_A.py
+++ part_A.py
@@ -2,74 +2,13 @@
 #TECH2 mandatory assignment - Part A
 
 
-from math import sqrt
+from math import sqrt #Import the built-in function in order to compute the square root
 
-# PArt 1: Using for loops
-def std_loops(x):
-    """
-    Compute standard deviation of x using loops.
+num_lst= [1,2,3,4,5]
 
-    Parameters
-    ----------
-    x : sequence of numbers
-        DESCRIPTION.
-
-    Returns
-    -------
-    sd : float
-        Standard deviation of the list of numbers.
-
-    """
- 
-    N = len(x)
-    mean = sum(x) / N
-    sum_sq_diff = sum((xi - mean) ** 2 for xi in x)
-    var = sum_sq_diff / N
-    return sqrt(var)
-
-
-#Part 2: Using built-in Functions
-def std_builtin(x):
-    """
-    Conpute standard deviation of x using the built in functions sum() and len().
-
-    Parameters
-    ----------
-    x : sequence of numbers
-        
-    Returns
-    -------
-    float
-        Standard deviation of the lsit of numbers
-
-    """
-    N = len(x)
-    mean = sum(x) / N
-    sum_sq = sum(xi ** 2 for xi in x)
-    var = (sum_sq /N ) - (mean ** 2)
-    return sqrt(var)
-
-
-# Part 3: using NumPy
-
-import numpy as np
-
-def std_numpy(x):
-    return np.std(x)
-
-
-#Demonstration of the three different ways
-
-num_lst = [1,2,3,4,5]
-
-#using loops
-std_loops_res = std_loops(num_lst)
-print(f'Standard Deviation using loops: {std_loops_res}')
-
-#Using built-in functions
-std_builtin_res = std_builtin(num_lst)
-print(f'Standard Deviation using built-in functions: {std_builtin_res}')
-
-#Using NumPy
-std_numpy_res = std_numpy(num_lst)
-print(f'Standard Deviation using NumPy: {std_numpy_res}')
+def std_dev(num_lst):
+    for num in num_lst:
+        N = 1/len(num_lst)
+        S = 1/N*(num * num**2)
+        var = sqrt(S - num**-2)
+    return std_dev(sqrt(var))
