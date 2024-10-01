19e9f45ac6bbb96a19640cd949e81d8dcc8b0193
olinesangro <Oline.Sangro@student.nhh.no>
2024-10-01 15:25:07 +0200

    finished 1/3 tasks in part A

--- part_A.py
+++ part_A.py
@@ -5,10 +5,24 @@
 from math import sqrt #Import the built-in function in order to compute the square root
 
 num_lst= [1,2,3,4,5]
+N = len(num_lst)
 
-def std_dev(num_lst):
+def std_dev(x):
+    
+    #Calculate the mean:
+    total = 0
     for num in num_lst:
-        N = 1/len(num_lst)
-        S = 1/N*(num * num**2)
-        var = sqrt(S - num**-2)
-    return std_dev(sqrt(var))
+        total += num
+    mean = total / N
+    
+    #Calculate the variance
+    var_sum = 0
+    for num in num_lst:
+        var_sum += (num - mean) ** 2
+        var = var_sum / N
+        
+        
+    #Calculate the standard deviation
+    return sqrt(var)
+
+print(std_dev(num_lst))
