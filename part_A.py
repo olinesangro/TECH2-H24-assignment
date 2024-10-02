dca4dd2e6b322cec7d5cd2002f67044bc0533ed7
olinesangro <Oline.Sangro@student.nhh.no>
2024-10-02 12:35:31 +0200

    Final verison of task A

--- part_A.py
+++ part_A.py
@@ -1,17 +1,16 @@
 
-#TECH2 mandatory assignment - Part A
+# TECH2 mandatory assignment - Part A
 
 from math import sqrt #Import the built-in function in order to compute the square root
 
 num_lst= [1,2,3,4,5]
 
 
-#FOR LOOPS
+# FOR LOOPS
 
 def std_loops(x):
     """
     
-
     Parameters
     ----------
     x : sequence of numbers
@@ -24,32 +23,31 @@ def std_loops(x):
     """
     N = len(num_lst)
     
-    #Calculate the mean:
-    total = 0
+    # Calculate the mean:
+    total = 0                   # Initialize total to o
     for num in num_lst:
-        total += num
+        total += num            # Add each number to total
     mean = total / N
     
-    #Calculate the variance
-    var_sum = 0
+    # Calculate the variance
+    var_sum = 0                         #Initialize var_sum to 0
     for num in num_lst:
-        var_sum += (num - mean) ** 2
+        var_sum += (num - mean) ** 2    #Add squared difference to var_sum
         var = var_sum / N
         
         
-    #Calculate the standard deviation
+    # Calculate the standard deviation
     return sqrt(var)
 
-print(std_loops(num_lst))
+print('Standard deviation using loops: ', std_loops(num_lst))
 
 
 
-#BUILT_IN FUNCTIONS: SUM and LEN
+# BUILT_IN FUNCTIONS: SUM and LEN
 
 def std_builtin(x):
     """
     
-
     Parameters
     ----------
     x : sequence of numbers
@@ -61,18 +59,19 @@ def std_builtin(x):
 
     """
 
-    N = len(x)
+    # Define N 
+    N = len(x)   
     
-    #Calculate the mean
+    # Calculate the mean
     mean = sum(x)/N
 
-    #Calculate the mean of squares
+    # Calculate the mean of squares
     mean_of_squares = sum(num**2 for num in x)/N
 
-    #Calculate the variance
+    # Calculate the variance
     var = mean_of_squares - mean ** 2
 
-    #Calculate the standard deviation
+    # Calculate the standard deviation
     return sqrt(var)
 
 print('Standard deviation using built in fuctions:' , std_builtin(num_lst))
@@ -80,10 +79,22 @@ print('Standard deviation using built in fuctions:' , std_builtin(num_lst))
 
 
 
-#NUMPY
+# NUMPY
 import numpy as np
 
 def std_numpy(x):
+    """
+    
+    Parameters
+    ----------
+    x : sequence of numbers
+
+    Returns
+    -------
+    float
+        Standars deviation of the list of numbers
+
+    """
     return np.std(x)
 
 print('Standard deviation using NumPY:', std_numpy(num_lst))
